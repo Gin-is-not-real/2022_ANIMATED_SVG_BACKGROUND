e43552b1398dbd1328bb7d8cfb58a8d82c1d8fff
Gin-is-not-real <ninapariat@live.fr>
2022-11-18 13:43:25 +0100

    js/ click on button made appear the cards-container, and a click on them make it disappear

--- anim.js
+++ anim.js
@@ -1,31 +1,44 @@
+// bg
+// let circleBgTop = document.querySelectorAll("#CIRCLE_BG_HALF_TOP");
+// circleBgTop.paths = document.querySelectorAll("#CIRCLE_BG_HALF_TOP path");
+// let circleBgBottom = document.querySelectorAll("#CIRCLE_BG_HALF_BOTTOM");
+// circleBgBottom.paths = document.querySelectorAll("#CIRCLE_BG_HALF_BOTTOM path");
+// let circleBgSmall1 = document.querySelectorAll("#CIRCLE_BG_SMALL_1");
+// circleBgSmall1.paths = document.querySelectorAll("#CIRCLE_BG_SMALL_1 path");
+// let circleBgSmall2 = document.querySelectorAll("#CIRCLE_BG_SMALL_2");
+// circleBgSmall2.paths = document.querySelectorAll("#CIRCLE_BG_SMALL_2 path");
+
 // faire clignoter le gros cercle en changeant sa classe pendant un temps defini
 let circle3 = document.querySelector("#CIRCLE_3");
 circle3.paths = document.querySelectorAll("#CIRCLE_3 path");
 
-// bg
-let circleBgTop = document.querySelectorAll("#CIRCLE_BG_HALF_TOP");
-circleBgTop.paths = document.querySelectorAll("#CIRCLE_BG_HALF_TOP path");
+// popup
+let svgContainer = document.querySelector('#svg-container');
+let contentContainer = document.querySelector('#content-container');
+let cardsContainer = document.querySelector('#cards-container');
 
-let circleBgBottom = document.querySelectorAll("#CIRCLE_BG_HALF_BOTTOM");
-circleBgBottom.paths = document.querySelectorAll("#CIRCLE_BG_HALF_BOTTOM path");
-
-let circleBgSmall1 = document.querySelectorAll("#CIRCLE_BG_SMALL_1");
-circleBgSmall1.paths = document.querySelectorAll("#CIRCLE_BG_SMALL_1 path");
-
-let circleBgSmall2 = document.querySelectorAll("#CIRCLE_BG_SMALL_2");
-circleBgSmall2.paths = document.querySelectorAll("#CIRCLE_BG_SMALL_2 path");
+let button = document.querySelector('#content-container input');
 
 //////////////////////////////////////////////////////
 // tests
-circle3FlashingLoop();
-// circlesBgFlashingLoop();
+console.log(button);
 
+button.addEventListener('click', function() {
+    cardsContainer.style.display = 'flex';
+
+    svgContainer.style.filter = 'blur(10px)';
+    contentContainer.style.filter = 'blur(10px)';
+})
+cardsContainer.addEventListener('click', function() {
+    cardsContainer.style.display = 'none';
+
+    svgContainer.style.filter = 'none';
+    contentContainer.style.filter = 'drop-shadow(1px 1px 2px rgba(47, 176, 188, 0.508))';
+})
 //////////////////////////////////////////////////////
 // MAIN
+circle3FlashingLoop();
 setInterval(circle3FlashingLoop, getRandomInt(7000, 15000));
-
-// setInterval(circlesBgFlashingLoop, getRandomInt(8000, 15000));
-
 
 //////////////////////////////////////////////////////
 // LOOPS
